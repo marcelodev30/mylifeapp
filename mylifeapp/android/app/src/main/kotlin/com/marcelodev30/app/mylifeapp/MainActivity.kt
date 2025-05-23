ddb3b70320d7526a612cd7a72d83e9d538f4ec9e
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2025-05-22 23:07:29 -0300

    add

---
+++ mylifeapp/android/app/src/main/kotlin/com/marcelodev30/app/mylifeapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.marcelodev30.app.mylifeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mylifeapp/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:mylifeapp/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
