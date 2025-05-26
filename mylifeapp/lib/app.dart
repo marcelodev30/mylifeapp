14aad619a1e59b90304647ee398a1e4fd58820ae
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2025-05-25 22:09:48 -0300

    ...

--- mylifeapp/lib/app.dart
+++ mylifeapp/lib/app.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'My Life App',
       theme: AppThemeData.dark,
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
